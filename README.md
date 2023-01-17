76782cabab1e07988fab0cca12825b21ed25bd67
VaibhavMojidra <vmojidra25@gmail.com>
2023-01-18 01:01:55 +0530

    Arithmetic Operators

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java Demo Arithmetic Operators
+
+[![Vaibhav Mojidra - output.png](https://raw.githubusercontent.com/VaibhavMojidra/Java---Demo-Arithmetic-Operators/master/output/output.png "Vaibhav Mojidra")](https://vaibhavmojidra.github.io/site/)
---
+++ src/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int n1=9,n2=2;
+		System.out.println("9 + 2 = "+(n1+n2));
+		System.out.println("9 - 2 = "+(n1-n2));
+		System.out.println("9 * 2 = "+(n1*n2));
+		System.out.println("9 / 2 = "+(n1/n2));
+		System.out.println("9 % 2 = "+(n1%n2));
+	}
+
+}
